1b01ce52ebcdf2d7179c22fe53a494c8fa42699e
Aman <54789531+eppisai@users.noreply.github.com>
2021-12-26 04:00:37 +0530

    DSA UI#2: used react split instead of split

--- src/Views/DSA/index.jsx
+++ src/Views/DSA/index.jsx
@@ -2,11 +2,28 @@ import React from "react"
 import "./index.css";
 import Sidebar from "./sidebar";
 import Footer from "./footer";
+import Split from "react-split";
+import Problem from "./problem";
+import Editor from "@monaco-editor/react";
 function DSA() {
 
 return(
     <div className="DSA">
     <Sidebar/>
+    <Split
+          sizes={[65, 35]}
+          direction="horizontal"
+          cursor="col-resize"
+          className="split-flex"        
+    >
+    <Problem/>
+    <Editor
+     height="calc(100vh - 2.4vh - 40px)"
+     theme="vs-dark"
+     defaultLanguage="javascript"
+     defaultValue="// some comment"
+    />
+    </Split>
     <Footer/>
     </div>
 )
---
+++ src/Views/DSA/problem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./problem.css"
+function Problem() {
+    return ( 
+        <div className="problem">
+        </div >
+     );
+}
+
+export default Problem;
