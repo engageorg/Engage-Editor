67ca4be13f9cd055b72625ef48c893be5048c5dd
Shantanu Shukla <shantanushukla64@gmail.com>
2021-12-30 17:46:25 +0530

    changes made in userCode screen according to session Auth

--- frontend/src/Views/SavedCode/userCode.jsx
+++ frontend/src/Views/SavedCode/userCode.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./style.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import Table from "../Table/savedCodeTable";
 const userCode = []
+
 export default function UserCode() {
   const [codeInfo, setCodeInfo] = useState([]);
+  const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
   useEffect(() => {
     // const submitButton = document.getElementById("openCode");
     // submitButton.addEventListener("click", () => {
@@ -25,9 +28,7 @@ export default function UserCode() {
     // })
    (() => {
       axios(
-        `http://localhost:5000/usercode/${JSON.parse(
-          localStorage.getItem("currentUserId")
-        )}`
+        `http://localhost:5000/usercode/${cookies.sessId}`
       ).then((response) => {
         response.data.code.forEach((code, index) => {
           userCode.push({code:code.code,no:index+1,id:code._id, name:code.name,language:code.language,createdAt:code.createdAt})
--- routes/userCode.js
+++ routes/userCode.js
@@ -1,24 +1,28 @@
-const express = require("express")
-const router = express.Router()
-const UserCode = require('../models/userCode')
-
-const firebase = require("firebase/app");
+const express = require("express");
+const router = express.Router();
+const UserCode = require("../models/userCode");
+const UserSession = require("../models/userSession");
 require("firebase/auth");
 
-router.get("/:id", async(req, res) => {
-    console.log(req.params)
-    UserCode.find({creator:req.params.id}).then(response => {
-        console.log(response)
-        res.json({status:200, code:response})
-    })
-})
+router.get("/:id", async (req, res) => {
+  console.log(req.params);
+  UserSession.find({ sessId: req.params.id }).then((response) => {
+    if (response.length === 1) {
+      UserCode.find({ creator: response[0].user }).then((response) => {
+        res.json({ status: 200, code: response });
+      });
+    } else {
+      console.log("NO SUCH USER");
+    }
+  });
+});
 
-router.get("/code/:id", async(req, res) => {
-    console.log(req.params)
-    UserCode.find({_id:req.params.id}, 'code').then(response => {
-        console.log(response[0].code)
-        res.json({status:200, code:response[0].code})
-    })
-})
+router.get("/code/:id", async (req, res) => {
+  console.log(req.params);
+  UserCode.find({ _id: req.params.id }, "code").then((response) => {
+    console.log(response[0].code);
+    res.json({ status: 200, code: response[0].code });
+  });
+});
 
-module.exports = router
+module.exports = router;
