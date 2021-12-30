e0aa52f0ab2cb005bb54c0e85d3b1f71c30abb39
Aman <54789531+eppisai@users.noreply.github.com>
2021-12-31 03:38:27 +0530

    username in redux from local storage

--- frontend/src/reducers/User.js
+++ frontend/src/reducers/User.js
@@ -1,8 +1,12 @@
-const username = (state = 'Engage User', action) => {
+var userName = 'Engage User';
+if(localStorage.getItem("engage-user")){
+   userName = localStorage.getItem("engage-user");
+}
+const username = (state = userName, action) => {
     switch(action.type){
       case "login":
         state = action.user;
-        console.log('name change')
+        localStorage.setItem("engage-user", action.user);
         return state;
       case "logout":
         state = "Engage User";
