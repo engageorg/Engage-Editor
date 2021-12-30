7320d848f7481acd98c69801c981385d56271bd4
Shantanu Shukla <shantanushukla64@gmail.com>
2021-12-31 00:44:59 +0530

    login and signUp page UI done

--- frontend/src/Views/Login/login.jsx
+++ frontend/src/Views/Login/login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import { useCookies } from "react-cookie";
+import './styles.css'
 
 export default function Login() {
   const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
@@ -25,9 +26,33 @@ export default function Login() {
   }, []);
   return (
     <>
-      <input type="email" id="email" placeholder="Email" />
-      <input type="password" id="password" placeholder="Password" />
-      <button id="submit">Login</button>
+
+			<div className="row full-height justify-content-center">
+				<div className="col-12 text-center align-self-center py-5">
+					<div>
+						<div className="card-3d-wrap mx-auto">
+							<div className="card-3d-wrapper">
+								<div className="card-front">
+									<div className="center-wrap">
+										<div className="section text-center">
+											<h4 className="mb-4 pb-3">Log In</h4>
+											<div className="form-group">
+												<input type="email" name="logemail" className="form-style" placeholder="Your Email" id="email" autoComplete="off"/>
+												<i className="input-icon uil uil-at"><i className="fas fa-at"></i></i>
+											</div>	
+											<div className="form-group mt-2">
+												<input type="password" name="logpass" className="form-style" placeholder="Your Password" id="password" autoComplete="off"/>
+												<i className="input-icon uil uil-lock-alt"><i className="fas fa-lock"></i></i>
+											</div>
+											<button id="submit" className="btn mt-4">submit</button>
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
     </>
   );
 }
--- frontend/src/Views/Signup/signUp.jsx
+++ frontend/src/Views/Signup/signUp.jsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React from "react";
 import axios from "axios";
-import {useEffect} from "react";
+import { useEffect } from "react";
 export default function SignUp() {
   useEffect(() => {
     const submitButton = document.getElementById("submit");
@@ -23,10 +23,61 @@ export default function SignUp() {
     });
   }, []);
   return (
-      <>
-      <input type="email" id="email" placeholder="Email" />
-      <input type="password" id="password" placeholder="Password" />
-      <button id="submit">Sign Up</button>
-      </>
+    <>
+      <div className="row full-height justify-content-center">
+        <div className="col-12 text-center align-self-center py-5">
+          <div>
+            <div className="card-3d-wrap mx-auto">
+              <div className="card-3d-wrapper">
+              <div className="card-front">
+        <div className="center-wrap">
+          <div className="section text-center">
+            <h4 className="mb-4 pb-3">Sign Up</h4>
+            <div className="form-group">
+              <input
+                type="text"
+                name="logname"
+                className="form-style"
+                placeholder="Your Full Name"
+                id="logname"
+                autoComplete="off"
+              />
+              <i className="input-icon uil uil-user"><i className="fas fa-user"></i></i>
+            </div>
+            <div className="form-group mt-2">
+              <input
+                type="email"
+                name="logemail"
+                className="form-style"
+                placeholder="Your Email"
+                id="email"
+                autoComplete="off"
+              />
+              <i className="input-icon uil uil-at"><i className="fas fa-at"></i></i>
+            </div>
+            <div className="form-group mt-2">
+              <input
+                type="password"
+                name="logpass"
+                className="form-style"
+                placeholder="Your Password"
+                id="password"
+                autoComplete="off"
+              />
+              <i className="input-icon uil uil-lock-alt"><i className="fas fa-lock"></i></i>
+            </div>
+            <button id="submit" className="btn mt-4">
+              submit
+            </button>
+          </div>
+        </div>
+      </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
