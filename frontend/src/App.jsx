97de005356f7d42a379d4dc339b7aa1134cb8867
Aman <54789531+eppisai@users.noreply.github.com>
2021-12-31 22:38:08 +0530

    some minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import allReducer from "./reducers";
-import DSA from "./Views/DSA";
-import SignUp from "./Views/Signup/signUp";
-import Login from './Views/Login/login';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import allReducer from './reducers';
+import DSA from './Views/DSA';
+import SignUp from './Views/Signup/';
+import Login from './Views/Login';
 import Problem from './Views/DSA/problem';
-import UserCode from "./Views/SavedCode/userCode";
-import SnippetScreen from "./Views/DSA/snippet";
+import UserCode from './Views/SavedCode/userCode';
+import SnippetScreen from './Views/DSA/snippet';
 
 const middleware = [thunk];
 
